836bf4d1caf3407e2150e1c6e6882e862fb6f9ca
zyxhaha <zyx930719@126.com>
2017-05-26 09:07:30 +0800

    首次

---
+++ demo/src/components/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="Login">
+     <input class="form-control" id="inputEmail3" placeholder="请输入账号" v-model="account">
+    <input type="password" class="form-control" id="inputPassword3" placeholder="请输入密码" v-model="password">
+    <button type="submit" class="btn btn-default" @click="login">登录</button>
+  </div>
+ 
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      account:"",
+      password:""
+    }
+  },
+  methods:{
+    login(){
+      this.$http.get('/api/login/getAccount').then((response)=>{ 
+        console.log(response);
+        let params={
+          account:this.account,
+          password:this.password
+        };  
+        console.log(this.$http.post);
+        return this.$http.post('/api/login/createAccout',params);
+      })
+      .then((response)=>{
+        console.log(response)
+      })
+      .catch((reject)=>{
+        console.log(reject)
+      });
+    }
+  }
+}
+</script>
+
+
